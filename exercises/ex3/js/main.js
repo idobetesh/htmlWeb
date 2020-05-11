ea078cc2739b3a157dda13d1296b1dd03f2d87bf
idobetesh <58806763+idobetesh@users.noreply.github.com>
2020-05-11 20:33:04 +0300

    ex3

---
+++ exercises/ex3/js/main.js
@@ -0,0 +1,38 @@
+function lenOfName() {
+    var fName = "ido";
+    var Lname = "betesh";
+    return fName.length * Lname.length;
+}
+
+function initializeRectangles() {
+    var articles = "";
+    for (i = 0; i < lenOfName(); i++) {
+        articles += "<article></article>";
+    }
+    var main = document.getElementsByTagName("main")[0];
+    main.innerHTML = articles;
+
+    var art = document.getElementsByTagName('article');
+    art[0].onmousemove = function () {
+        art[0].style.backgroundImage = "url('./images/IDO.png')";
+    }
+
+    art[0].onmouseout = function () {
+        art[0].style.backgroundImage = 'none';
+        art[0].style.backgroundColor = 'rgb(221, 163, 157)';
+    }
+}
+
+function press() {
+    var rects = document.getElementsByTagName('article');
+    for (i = 0; i < "ido".length; i++) {
+        rects[i].style.backgroundColor = '#2d3436';
+    }
+}
+
+function pressBack() {
+    var rects = document.getElementsByTagName('article');
+    for (i = 0; i < "ido".length; i++) {
+        rects[i].style.backgroundColor = 'rgb(221, 163, 157)';
+    }
+}
